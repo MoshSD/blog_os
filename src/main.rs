1f968269b903fd77e0d55790eacc6c653fd99d09
MoshSD <62759350+MoshSD@users.noreply.github.com>
2022-09-01 03:17:51 +0100

    -Finished up the vga_buffer.rs for basic printing to console

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,9 @@
 
 mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]  //disabling mangling for this fn so that it can be used by the linker - linker will look for fn named _start without main
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop{}
 }
