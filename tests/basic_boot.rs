d45b46d1d1ffefdc024cb29664aef60eb4fc006d
MoshSD <62759350+MoshSD@users.noreply.github.com>
2022-09-08 21:31:32 +0100

    - Finished up unit and integration testing

---
+++ tests/basic_boot.rs
@@ -0,0 +1,33 @@
+// in tests/basic_boot.rs
+
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![reexport_test_harness_main = "test_main"]
+#![test_runner(blog_os::test_runner)]
+
+use blog_os::println;
+use core::panic::PanicInfo;
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+
+fn test_runner(tests: &[&dyn Fn()]) {
+    unimplemented!();
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
+}
+
+
+//Testing whether VGA print works within a basic boot environment 
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
